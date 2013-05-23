ecae665a81dbb2d374ed92cb48acab698c003e9e
ff4199 <yangfeiyuok@gmail.com>
2013-05-23 16:53:22 +0800

    Initial commmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ordermeal::Application.config.secret_token = '932ef5c571f3ada8d69d3f639721a6799b5a6b7467e9c7c8c4a4397a26ac5066e10b8f8bbe2288fbb85b2ef623168660178acaa16fdbc60ae9dced65c1c8638b'
